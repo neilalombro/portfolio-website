f85bec22d81ab1b348bf0005ab2f79ef646eb34f
Neil Alombro <nfa44@uclive.ac.nz>
2024-04-01 21:01:11 +1300

    Added analysing

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
 
+    <Analytics />
     <SpeedInsights />
+    
     <RouterView />
     
 </template>
 
 <script setup lang="ts">
 
+import { Analytics } from "@vercel/analytics"
 import { SpeedInsights } from "@vercel/speed-insights/vue"
 import { RouterView } from "vue-router"
 
