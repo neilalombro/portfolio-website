7c3b155df516bded066970c5b009284d407282c6
Neil Alombro <nfa44@uclive.ac.nz>
2024-04-01 14:33:22 +1300

    Fixed image sizing and squashing on phones

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -8,15 +8,15 @@
 
         <div id="links">
             <a href="https://nz.linkedin.com/in/neil-alombro" target="_blank">
-                <img src="/images/contacts/linkedin.png" alt="LinkedIn" class="dark:filter dark:invert">
+                <img src="/images/contacts/linkedin.png" alt="LinkedIn" class="dark:filter dark:invert w-7 h-7">
             </a>
 
             <a href="https://github.com/neilalombro" target="_blank">
-                <img src="/images/contacts/github.png" alt="GitHub icon" class="dark:filter dark:invert">
+                <img src="/images/contacts/github.png" alt="GitHub icon" class="dark:filter dark:invert w-7 h-7">
             </a>
 
             <a href="mailto:neilalombro@gmail.com">
-                <img src="/images/contacts/email.png" alt="Envelope icon" class="dark:filter dark:invert">
+                <img src="/images/contacts/email.png" alt="Envelope icon" class="dark:filter dark:invert w-7 h-7">
             </a>
         </div>
     </div>
